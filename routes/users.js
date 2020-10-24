aeedb0a97d9a0545168dcd2b8cb5a98f09c9ee9f
vietnguyenhoangw <hoang-viet.nguyen@ant-tech.eu>
2020-10-25 00:31:50 +0700

    Rounter testing

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/user', function(req, res) {
+  res.send('respond with /user endpoint')
+})
+
 module.exports = router;
